7cb20a611564f7d08ae04b1f9503101e7c3660c6
Vishal <er.vishalnagwde@gmail.com>
2022-07-15 23:19:48 +0530

    Hii

--- JavaBasicProgramming/src/vishal_package/Test_Class.java
+++ JavaBasicProgramming/src/vishal_package/Test_Class.java
@@ -2,6 +2,6 @@ package vishal_package;
 
 public class Test_Class {
  public static void main(String[] args) {
-	System.out.println("Good Night Sir.Its my 1st program ");
+	System.out.println("Good Night Sir.Its my 1st Pograme");
 }
 }
