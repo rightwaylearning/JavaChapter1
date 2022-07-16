136bc449347e0d5f95b16011041eb105baa32a7a
Kalpesh <Kalpesh@gmail.com>
2022-07-16 20:09:42 +0530

    new program

---
+++ JavaBasicProgramming/src/kalpesh_package/Address.java
@@ -0,0 +1,16 @@
+package kalpesh_package;
+
+public class Address 
+{
+
+String homeNumber;
+int HouseNumber;
+String City ;
+String Country;
+
+	
+}
+
+
+
+
---
+++ JavaBasicProgramming/src/kalpesh_package/Controller.java
@@ -0,0 +1,27 @@
+package kalpesh_package;
+
+public class Controller {
+
+	
+public static void main(String[] args) {
+	{
+}
+
+Address d = new Address();
+
+     d.homeNumber = "c-102";
+     d.HouseNumber = 11;
+     d.City = "Pune";
+     d.Country = "Maha";
+	}
+	
+	
+	
+	
+
+	
+}
+
+	
+	
+
