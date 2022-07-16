114e45b5f8ea34f21bd100dcfb928cc7150ff56e
Kalpesh <Kalpesh@gmail.com>
2022-07-16 12:46:30 +0530

    updated program

--- JavaBasicProgramming/src/kalpesh_package/Test.java
+++ JavaBasicProgramming/src/kalpesh_package/Test.java
@@ -4,5 +4,7 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("kalpesh");
 	System.out.println("Hi kaplpesh how are u");
+	System.out.println("Hi I am good");
+	
 }
 }
