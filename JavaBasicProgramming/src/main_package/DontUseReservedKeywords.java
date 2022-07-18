584b7466b2bc62839b74cc0701a749787fdd5821
sachin <abc@gmail.com>
2022-07-18 22:52:55 +0530

    first day session

---
+++ JavaBasicProgramming/src/main_package/DontUseReservedKeywords.java
@@ -0,0 +1,25 @@
+package main_package;
+
+public class DontUseReservedKeywords {
+
+	int class = 2;
+	String void ="abc";
+	
+	int package = 3;
+	
+	float public = 3.4F;
+	
+	String name = "mahesh";
+	
+	int String = 23;  // don't use pre defined classes name as property name
+	
+	int Collection = 23;
+	
+	
+	
+	
+	
+	
+	
+	
+}
