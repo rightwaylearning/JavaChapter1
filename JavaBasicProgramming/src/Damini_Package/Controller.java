699fdc17d90732be2c94900765cd5212ec97cdf4
damini <daminiupadhyay1993@gmail.com>
2022-07-21 11:06:58 +0530

    return type

---
+++ JavaBasicProgramming/src/Damini_Package/Controller.java
@@ -0,0 +1,20 @@
+package Damini_Package;
+
+public class Controller {
+	public static void main(String[] data) 
+	
+	{
+		Test t=new Test();
+	    int r=t.doMul(20, 5);
+	    System.out.println(r);
+	      
+	       Exam e=t.getData();
+	       
+	       System.out.println(e.age);
+	       System.out.println(e.name);
+		
+	}
+				
+	
+
+}
