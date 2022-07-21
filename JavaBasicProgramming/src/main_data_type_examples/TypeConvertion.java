197d37aa83aefe2ad2022647f2f874d4480f23d1
sachin <abc@gmail.com>
2022-07-21 22:53:15 +0530

    data type

--- JavaBasicProgramming/src/main_data_type_examples/TypeConvertion.java
+++ JavaBasicProgramming/src/main_data_type_examples/TypeConvertion.java
@@ -4,14 +4,26 @@ public class TypeConvertion {
 
 	
 	public static void main(String[] args) {
-		
 		byte b = 10;
 		short s = b;  // type casting >> compiler
 		
 		short s1 = 10;
-		byte b1 = s1;  // as per size calculation compiler given error
+//		byte b1 = s1;  // as per size calculation compiler given error
 		
 		long l = 3276L;
-		int i = l;  // same type missmatch
+//		int i = l;  // same type missmatch
+		
+		
+		for(int i =0 ;i<=200;i++) {
+			char ch =(char)i;
+			System.out.println(i+"  >>  "+ch);
+		}
+		
+		float f = 45768.8936475647345734F;
+		System.out.println(f);
+		
+		double d = 45768.8936475647345734;
+		System.out.println(d);
+		
 	}
 }
