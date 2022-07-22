a5f94d0ceaee7e2e4d8eb4fd785b25997ca4565a
Akshay Tejale <akshaytejale@gmail.com>
2022-07-22 19:56:41 +0530

    Student_class

---
+++ JavaBasicProgramming/src/Akshay_New_Package/Student.java
@@ -0,0 +1,16 @@
+package Akshay_New_Package;
+
+public class Student {
+	public static void main(String[] args) {
+	
+	int rollNumber = 23;
+	String Student_Name = "Akshay Vilas Tejale";
+	int Student_ID = 23;
+	long phone_Number = 9673717091l;
+	
+		System.out.println("Student Roll Number - " + rollNumber);
+		System.out.println("Student Name - " + Student_Name);
+		System.out.println("Student ID - " + Student_ID);
+		System.out.println("Student Roll Number - " + phone_Number);
+}
+}
