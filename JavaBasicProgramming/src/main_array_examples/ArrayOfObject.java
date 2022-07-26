626bd6cd8ee05b4357d9936faf768e569d2cddfd
sachin <abc@gmail.com>
2022-07-26 22:50:55 +0530

    array & object example

---
+++ JavaBasicProgramming/src/main_array_examples/ArrayOfObject.java
@@ -0,0 +1,23 @@
+package main_array_examples;
+
+public class ArrayOfObject {
+	public static void main(String[] args) {
+
+		Student[] arr = new Student[5];
+
+		// way 1 to assign value to array
+		arr[0] = new Student();
+						arr[0].name = "AB";
+						arr[0].rollNumber = 123;
+						
+   // ----------------------------------						
+		// way 2 to assign value to array		
+				Student s = new Student();
+					s.name = "WW";
+					s.rollNumber = 23;
+				
+		arr[1] = s;
+		
+
+	}
+}
