0b0d1bf5dd223bd7edc8ffd3bc7c7b5748ae6bef
Akshay Tejale <akshaytejale@gmail.com>
2022-07-26 21:47:00 +0530

    Basic_Of_ArrayExamples

---
+++ JavaBasicProgramming/src/Akshay_New_Package/ColorsOfArray.java
@@ -0,0 +1,11 @@
+package Akshay_New_Package;
+
+public class ColorsOfArray {
+	
+	String name;
+	
+	void showMyName () {
+		System.out.println(this.name);
+	}
+
+}
---
+++ JavaBasicProgramming/src/Sahil_main/Sahil_Test.java
@@ -0,0 +1,10 @@
+package Sahil_main;
+
+public class Sahil_Test { 
+	
+	int doadd (int a, int b) {
+		
+	int c = a + b;
+	return c;
+}
+}
