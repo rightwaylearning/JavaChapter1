7063f61674b541f2bd334989bdc88713d4b52064
damini <daminiupadhyay1993@gmail.com>
2022-07-27 16:26:45 +0530

    this keyword

---
+++ JavaBasicProgramming/src/Damini_Package/Student.java
@@ -0,0 +1,10 @@
+package Damini_Package;
+
+public class Student {
+	String name;
+
+	void showName()
+	{
+		System.out.println(this.name);
+	}
+}
---
+++ JavaBasicProgramming/src/Damini_main/Runner.java
@@ -0,0 +1,11 @@
+package Damini_main;
+
+public class Runner {
+	public static void main(String[] args) {
+		
+		User u=new User();
+		
+		u.doMul(20, 4);
+		u.doSub(30, 5);
+	}
+}
