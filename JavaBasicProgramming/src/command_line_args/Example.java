e12de10c6a928dba4b59ea99efcfa74d2af045b0
sachin <abc@gmail.com>
2022-07-31 08:54:25 +0530

    2 d array example & cmd argrs example

---
+++ JavaBasicProgramming/src/command_line_args/Example.java
@@ -0,0 +1,28 @@
+package command_line_args;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+//
+//		int num1  = Integer.parseInt(args[0]);
+//		int num2  = Integer.parseInt(args[1]);
+//		
+//	    int sum = num1 + num2;
+//	    
+//	    System.out.println(sum);
+	 
+	// ---------------------------------------------------					
+//		int num1  = Integer.parseInt(args[0]);
+//		int num2  = Integer.parseInt(args[1]);
+//		
+//	    
+//	    System.out.println(num1 + num2);
+		
+	//-------------------------------------------------
+		    
+	    System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+	    
+	    
+	}
+}
