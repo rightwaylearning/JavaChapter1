2f2d3f6adf9b745e73e40cf88e3e5cbb00b6241a
Akshay Tejale <akshaytejale@gmail.com>
2022-07-31 15:55:28 +0530

    Arithmatic_Akshay

---
+++ LogicBuiding/src/Arithmatic_Akshay/Arithmatic_Operators.java
@@ -0,0 +1,83 @@
+package Arithmatic_Akshay;
+
+public class Arithmatic_Operators {
+	
+	public static void main(String[] args) {
+		
+//-----------------------------------------------------------------------------------------------
+//		byte & int
+		
+		byte a = 15;
+		byte b = 20;
+		
+//		short sum = a+b;
+		int sum = a+b;
+		
+		System.out.println(sum);
+		System.out.println("-----------------------------------");
+		
+//		a & b is byte and after byte "int" is max hence "int sum = a+b;
+		
+//-----------------------------------------------------------------------------------------------
+//		short & int
+		
+		short a1 = 25;
+		short a2 = 5;
+		
+		int sum1 = a1+a2;
+		
+		System.out.println(sum1);
+		System.out.println("-----------------------------------");
+		
+//-----------------------------------------------------------------------------------------------
+		
+//		int & long
+		
+		int b1 = 12;
+		int b2 = 12;
+		
+		long sum2 = b1 + b2;
+		
+		System.out.println(sum2);
+		System.out.println("-----------------------------------");
+		
+//-----------------------------------------------------------------------------------------------
+		
+// long & float
+		
+		long c1 = 9673717091L;
+		long c2 = 9045720311L;
+		
+		float c3 = c1+c2;
+		
+		System.out.println(c3);
+		System.out.println("-----------------------------------");
+		
+// float & double
+		
+		float d1 = 20.5f;
+		float d2 = 15.5f;
+		
+		double d3 = d1+d2;
+		
+		System.out.println(d3);
+		System.out.println("-----------------------------------");
+		
+//-----------------------------------------------------------------------------------------------
+				
+// char & int
+		
+		char e1 = 'A';
+		char e2 = 'B';
+		
+		int e3 = e1+e2;
+		
+		System.out.println(e3);
+		System.out.println("-----------------------------------");
+		
+		
+		
+		
+	}
+
+}
