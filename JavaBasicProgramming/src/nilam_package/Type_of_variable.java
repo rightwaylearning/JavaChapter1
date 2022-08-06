4a0765ee63165e58e750c510fa96f1dd7258d258
nilamnagwade <pacharnenn244@gmail.com>
2022-08-06 15:36:26 +0530

    type of variable

---
+++ JavaBasicProgramming/src/nilam_package/Type_of_variable.java
@@ -0,0 +1,17 @@
+package nilam_package;
+
+public class Type_of_variable {
+	
+	int rollNumber;
+	String name;
+	static String schoolname;
+	 
+	void displayinfo() {
+		 System.out.println(rollNumber);
+		 System.out.println(name);
+		 System.out.println(Type_of_variable.schoolname);
+	}
+	
+	static void 
+
+}
