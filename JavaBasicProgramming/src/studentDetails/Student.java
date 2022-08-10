f6310701005f86f5777f0f0b42bf650be6b11380
damini <daminiupadhyay1993@gmail.com>
2022-08-10 21:55:05 +0530

    shortcircuit

---
+++ JavaBasicProgramming/src/studentDetails/Student.java
@@ -0,0 +1,11 @@
+package studentDetails;
+import Damini_TestDetails.Test;
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		Test t =new Test();
+		
+	}	
+
+}
---
+++ LogicBuiding/src/Damini/shortCircuitOperatoer.java
@@ -0,0 +1,17 @@
+package Damini;
+
+public class shortCircuitOperatoer {
+	
+	  boolean m1()
+	{
+		  System.out.println("m1 method call");
+		return 	true;
+	} 
+	  boolean m2()
+	  {
+		  System.out.println("m2 method call");
+		  
+		  return false;
+	  }
+
+}
