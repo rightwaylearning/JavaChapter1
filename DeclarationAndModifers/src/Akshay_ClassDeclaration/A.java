f3d6edf3f12143218ce3b73aaaf6f816709ecd66
Akshay Tejale <akshaytejale@gmail.com>
2022-08-11 23:17:19 +0530

    get_set

---
+++ DeclarationAndModifers/src/Akshay_ClassDeclaration/A.java
@@ -0,0 +1,22 @@
+package Akshay_ClassDeclaration;
+
+public class A {
+	
+	public static void main(String[] args) {
+		System.out.println("I am a Akshay");
+	}
+
+	class B {
+		
+	}
+	
+	class C {
+		
+	}
+	
+	class d {
+		
+	}
+}
+
+// Here we can use multiple classes.
