a10865ad107687f6636e3725df61d7bf10f15d3a
Sahil <Sahil@gmail.com>
2022-08-23 06:40:37 +0530

    abstract parent child

---
+++ AbstractClass&Interface/src/sahil_AbstractClassInterface/child.java
@@ -0,0 +1,8 @@
+package sahil_AbstractClassInterface;
+
+public class child  extends parent{
+
+	void m2 () {
+		System.out.println("child m2 method");
+	}
+}
---
+++ AbstractClass&Interface/src/sahil_AbstractClassInterface/parent.java
@@ -0,0 +1,8 @@
+package sahil_AbstractClassInterface;
+
+public class parent {
+
+	void m1() {
+		System.out.println("m1 is parent method");
+	}
+}
