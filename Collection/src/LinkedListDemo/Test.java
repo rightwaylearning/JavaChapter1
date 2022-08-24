5a4202af028b817b1d6378f2a58403dba8a6152d
sachin <abc@gmail.com>
2022-08-24 09:04:12 +0530

    LL & AL

---
+++ Collection/src/LinkedListDemo/Test.java
@@ -0,0 +1,59 @@
+package LinkedListDemo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+class Student {
+	String name;
+	int rollNumber;
+	float percenatge;
+	
+	public Student(String name, int rollNumber, float percenatge) {
+		super();
+		this.name = name;
+		this.rollNumber = rollNumber;
+		this.percenatge = percenatge;
+	}
+
+	@Override
+	public String toString() {
+		return "{"+ name + ", " + rollNumber + "," + percenatge + "}";
+	}
+	
+}
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Student s= new Student("AB",23,78.00F);
+		System.out.println(s);  // s.toString();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		LinkedList<Integer> ll = new LinkedList<>();
+		
+		ll.add(10);
+		ll.add(20);
+		ll.add(50);
+		ll.add(40);
+		ll.add(50);
+		ll.add(null);
+		
+		ArrayList<Integer> al = new ArrayList<>(ll);
+		
+		System.out.println(ll.toString());
+		
+	}
+}
