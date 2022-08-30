844db85e3bea667e7496daaea172c2e1f6a35d37
pratik <pratiksupekar2050@gmail.com>
2022-08-30 21:31:32 +0530

    collection employee plane array program

---
+++ Collection/src/pratik_collection_empArray/Employee.java
@@ -0,0 +1,40 @@
+package pratik_collection_empArray;
+
+public class Employee {
+private String empName;
+private Integer empId;
+private Float empSalary;
+public Employee(String empName, Integer empId, Float empSalary) {
+	super();
+	this.empName = empName;
+	this.empId = empId;
+	this.empSalary = empSalary;
+}
+public Employee() {
+	
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public Integer getEmpId() {
+	return empId;
+}
+public void setEmpId(Integer empId) {
+	this.empId = empId;
+}
+public Float getEmpSalary() {
+	return empSalary;
+}
+public void setEmpSalary(Float empSalary) {
+	this.empSalary = empSalary;
+}
+@Override
+public String toString() {
+	return "[" + empName + "," + empId + "," + empSalary + "]";
+}
+
+
+}
