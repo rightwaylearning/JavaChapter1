c962cd883fc3040a19e8e7ac0c9baf6e5080cba3
sachin <abc@gmail.com>
2022-08-31 08:58:25 +0530

    Vector & assigment

---
+++ Collection/src/assignment31_08_2022/Employee.java
@@ -0,0 +1,48 @@
+package assignment31_08_2022;
+
+public class Employee {
+
+	private String employeeName;
+	private Integer employeeId;
+	private Double employeeSalary;
+
+	public Employee(String employeeName, Integer employeeId, Double employeeSalary) {
+		super();
+		this.employeeName = employeeName;
+		this.employeeId = employeeId;
+		this.employeeSalary = employeeSalary;
+	}
+
+	public Employee() {}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeName=" + employeeName + ", employeeId=" + employeeId + ", employeeSalary="
+				+ employeeSalary + "]";
+	}
+	
+}
---
+++ Collection/src/assignment31_08_2022/ShowList.java
@@ -0,0 +1,10 @@
+package assignment31_08_2022;
+
+public class ShowList {
+
+	static void show(Employee[] arr) {
+		for(Employee e:arr) {
+			System.out.println(e);
+		}
+	}
+}
