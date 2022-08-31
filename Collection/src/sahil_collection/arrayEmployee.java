b15b824827e949aaeec99d32b0d8e4a49aec2b2d
Sahil <Sahil@gmail.com>
2022-08-31 07:44:06 +0530

    collection

---
+++ Collection/src/sahil_collection/arrayEmployee.java
@@ -0,0 +1,46 @@
+package sahil_collection;
+
+public class arrayEmployee {
+  String Name;
+  int ID ;
+   double salary;
+  
+  void sahil (){
+	 System.out.println(Name);
+	  System.out.println(ID);
+  System.out.println(salary);
+     System.out.println("=======================");
+	  
+  }
+
+public String getName() {
+	return Name;
+}
+
+public void setName(String name) {
+	Name = name;
+}
+
+public int getID() {
+	return ID;
+}
+
+public void setID(int iD) {
+	ID = iD;
+}
+
+public double getSalary() {
+	return salary;
+}
+
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+
+
+}
+
+
+  
+  
+
