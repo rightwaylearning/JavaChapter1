6ef6688aa73523bbd3fd75ade33bcfcd9e38ccd0
sachin <abc@gmail.com>
2022-09-02 21:19:59 +0530

    method signature code

---
+++ ObjectOrintedConcept/src/object/orinted/examples/MethodSignature.java
@@ -0,0 +1,15 @@
+package object.orinted.examples;
+
+public class MethodSignature {
+
+	void m1(int a, int b) {
+		
+	}
+	
+	int m1(int x, int y) {
+		return 0;
+	}
+}
+
+// return type is not part of method signature
+// 2 same method signature under one class not allow , it will give C.E
