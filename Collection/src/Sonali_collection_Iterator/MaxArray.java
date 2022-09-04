85daa8f40e097227360bf665de9324293987344b
sonal galande <sonaligalande>
2022-09-04 09:33:43 +0530

    commit code

--- Collection/src/Sonali_collection_Iterator/MaxArray.java
+++ Collection/src/Sonali_collection_Iterator/MaxArray.java
@@ -5,7 +5,7 @@ public class MaxArray {
 	public static void main(String[] args) {
 		
 		 
-        int [] arr = new int [] {20,40,50,40,300,500,400,300,500};  
+        int [] arr = new int [] {20,40,50,40,300,400,300,500};  
         
         int max = arr[0];  
        
@@ -14,7 +14,7 @@ public class MaxArray {
            if(arr[i] > max)  
                max = arr[i];  
         }  
-        System.out.println("Largest element present in given array: " + max); 
+      //  System.out.println("Largest element present in given array: " + max); 
 	
         
 	
@@ -25,8 +25,17 @@ public class MaxArray {
          if(arr[i] >Second_max)  
             Second_max = arr[i];  
       }  
-      System.out.println("Largest element present in given array: " + max); 
-      System.out.println("Largest element present in given array: " + Second_max);
+      System.out.println(Second_max);
+     
+      int third_max=arr[0];
+      for(int i=0;i<arr.length;i++) {
+    	  if(arr[i]>third_max)
+    		  third_max=arr[i];
+    	  
+      }
+     // System.out.println("Largest element present in given array:"+third_max);
+      //System.out.println("Largest element present in given array: " + max); 
+      //System.out.println("Largest element present in given array: " + Second_max);
 	
       
 }
