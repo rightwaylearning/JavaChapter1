c604701bdcbd3fa9229a28999ddda82b151c8057
sachin.lagad <sachin.lagad@gmail.com>
2022-09-07 21:25:35 +0530

    inheritance

---
+++ ObjectOrintedConcept/src/Inheritance_07_09_2022/TypeOfInheritance.java
@@ -0,0 +1,18 @@
+package Inheritance_07_09_2022;
+
+class A{}
+class B{}
+
+class C extends A,B{
+	
+}
+
+
+
+
+public class TypeOfInheritance {
+
+	public static void main(String[] args) {
+		
+	}
+}
