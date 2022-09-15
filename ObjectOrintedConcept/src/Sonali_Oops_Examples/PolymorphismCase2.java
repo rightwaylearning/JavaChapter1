ee39a650d46c167d09ddf5de6ae1f201dc152d8c
sonal galande <sonaligalande>
2022-09-15 21:33:40 +0530

    commited

--- ObjectOrintedConcept/src/Sonali_Oops_Examples/PolymorphismCase2.java
+++ ObjectOrintedConcept/src/Sonali_Oops_Examples/PolymorphismCase2.java
@@ -1,24 +1,31 @@
 package Sonali_Oops_Examples;
-
-
 class B{
-	void m2(int a , double b) {
-		System.out.println("int-double args method ");
-		
+	void m1(int a) {
+		System.out.println("int args method");
 	}
-	void m2(double a , int b) {
-		System.out.println("double-int args method");
-		
+	void m1(short s) {
+		System.out.println("short args method");
+	}
+	void m1(double z) {
+		System.out.println("int args method");
+	}
+	void m1(byte b) {
+		System.out.println("short args method");
 	}
 }
-public class PolymorphismCase2 {
 
+public class PolymorphismCase2 {
 	public static void main(String[] args) {
 		B b = new B();
-		byte p=5;
-		b.m2(20.0, p);
-		b.m2(p,5.5);
+		byte c=20;
+		b.m1(c);
 		
+		short s =67;
+		b.m1(s);
 		
+		 
+        long l = 9099090909L;
+        b.m1(l);
 	}
+
 }
