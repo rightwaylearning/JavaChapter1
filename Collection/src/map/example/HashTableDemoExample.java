ee1c35b13e6e4be18490e033df608b3db894016d
sachin.lagad <sachin.lagad@gmail.com>
2022-09-20 08:54:39 +0530

    poly

---
+++ Collection/src/map/example/HashTableDemoExample.java
@@ -0,0 +1,22 @@
+package map.example;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.TreeSet;
+
+public class HashTableDemoExample {
+	  
+	public static void main(String[] args) {
+		
+		TreeSet<Integer> t = new TreeS
+		ht.put(1, "AA");
+		ht.put(2, "BB");
+		ht.put(3, "CC");
+		ht.put(4, "null");
+		ht.put(5, "EE");
+		ht.put(12, "EE");
+	
+		System.out.println(ht);
+	}
+
+}
