892f55fc4c535b93a805ae05d213fdd454241bae
sonal galande <sonaligalande24@gmail.com>
2022-09-28 20:28:28 +0530

    java.lang

---
+++ FileHandling/src/Sonali_File_Handling/CommandLineargs.java
@@ -0,0 +1,9 @@
+package Sonali_File_Handling;
+
+public class CommandLineargs {
+public static void main(String[] args) {
+	int num1 = Integer.parseInt(args[0]);
+	int num2 = Integer.parseInt(args[1]);
+	System.out.println(num1 + num2);
+}
+}
---
+++ FileHandling/src/Sonali_File_Handling/ConsoleClassDemo.java
@@ -0,0 +1,14 @@
+package Sonali_File_Handling;
+
+import java.io.Console;
+
+public class ConsoleClassDemo {
+	public static void main(String[] args) {
+		 Console c = System.console();
+		 String s = c.readLine();
+		 System.out.println(s);
+		 
+		
+	}
+
+}
