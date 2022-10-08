da202b4bc5c5bbe6b8247589c79e08013ee6a621
ahinavejivan <jivanahinave14@gmail.com>
2022-10-08 08:56:54 +0530

    PatternProgramming

---
+++ ExceptionHandling_Jivan/src/CustomExceptionHandling/AgeLimitException.java
@@ -0,0 +1,7 @@
+package CustomExceptionHandling;
+
+public class AgeLimitException extends Throwable{
+	
+	AgeLimitException
+
+}
---
+++ SystemInputDeviceReaderClasses_Jivan/src/SystemInputReader_Jivan/ScannerClassDemo.java
@@ -0,0 +1,17 @@
+package SystemInputReader_Jivan;
+
+import java.util.Scanner;
+
+public class ScannerClassDemo { 
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second number");
+		int num2 = sc.nextInt();
+		System.out.println(num1 + num2);
+	}
+}
+
