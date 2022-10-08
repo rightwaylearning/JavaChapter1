7df80165dd62db94388e562e1b09f97dc7d436a5
sachin lagad <sachin.lagad@gmail.com>
2022-10-08 08:54:29 +0530

    cutom exception handling

---
+++ ExceptionHandling/src/AgeLimitEception.java
@@ -0,0 +1,7 @@
+
+public class AgeLimitEception extends RuntimeException {
+
+	AgeLimitEception(){
+		super("AgeLimitException , because of age is leass than 18 Or 21");
+	}
+}
---
+++ ExceptionHandling/src/basic/info/BasicExceptionhandlingDemo.java
@@ -0,0 +1,12 @@
+package basic.info;
+
+public class BasicExceptionhandlingDemo {
+
+	public static void main(String[] args) {
+		try {
+		  int i = 10/0;
+		}catch(Exception e) {
+			 System.out.println(e);
+		}
+	}
+}
